38a9c792aae726ffeffbb56ede1b054eebcbac1e
ruilinchu <ruilinchu@gmail.com>
2018-02-20 13:59:16 -0800

    Update getCluster.m

--- getCluster.m
+++ getCluster.m
@@ -2,7 +2,7 @@ function cluster = getCluster(sbatch_arg)
   % input is slurm sbatch arguments minus ntasks, e.g.:
   % '--time 24:00:00 --partition scec --mem-per-cpu 2G', list goes on
 
-evalc('system(''mkdir -p ~/MATLAB_JOB_STORAGE'')');
+evalc('system(''export TZ=America/Los_Angeles && mkdir -p ~/MATLAB_JOB_STORAGE'')');
 cluster = parallel.cluster.Generic('JobStorageLocation', '~/MATLAB_JOB_STORAGE');
 set(cluster, 'HasSharedFilesystem', true);
 set(cluster, 'ClusterMatlabRoot', '/usr/usc/matlab/default/');

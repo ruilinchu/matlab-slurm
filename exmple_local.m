279f36026644243a56bb6735cb2576de898fec30
ruilinchu <ruilinchu@gmail.com>
2018-02-20 12:59:25 -0800

    Update exmple_local.m

--- exmple_local.m
+++ exmple_local.m
@@ -1,4 +1,3 @@
-tic
 c=parallel.cluster.Local();
 evalc('system(''export TZ=America/Los_Angeles && mkdir -p ~/MATLAB_JOB_STORAGE_local'')');
 c.JobStorageLocation='~/MATLAB_JOB_STORAGE_local';
@@ -8,7 +7,7 @@ n=str2num(a);
 c.NumWorkers=n;
 
 c.parpool(n)
-toc
+%do parfor
 
 delete(gcp)
 

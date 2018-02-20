264ec34565b0541032060715a238b3a83aaf12a5
ruilinchu <ruilinchu@gmail.com>
2018-02-20 14:40:58 -0800

    Create example_slurm.m

---
+++ example_slurm.m
@@ -0,0 +1,29 @@
+c = get_SLURM_cluster('-t 24:00:00 -p main');
+
+c.parpool(8) %request ntasks=8
+
+spmd
+switch labindex 
+ case 1
+  A = 1:5;
+  labSend(A,2,199);
+ case 2
+  A = labReceive(1,199);
+ otherwise
+  A = 11:15;
+ end
+end
+
+A{:}
+
+spmd
+if labindex == 1
+  A=labBroadcast(1,1:5);
+ else
+  A=labBroadcast(1);
+end
+end
+
+A{:}
+
+delete(gcp)

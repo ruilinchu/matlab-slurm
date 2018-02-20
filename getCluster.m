0fe625f0c54521eb4b8bef4443315400ebe96653
ruilinchu <ruilinchu@gmail.com>
2018-02-20 12:15:09 -0800

    Create getCluster.m

---
+++ getCluster.m
@@ -0,0 +1,16 @@
+function cluster = getCluster(sbatch_arg)
+  % input is slurm sbatch arguments minus ntasks, e.g.:
+  % '--time 24:00:00 --partition scec --mem-per-cpu 2G', list goes on
+
+evalc('system(''mkdir -p ~/MATLAB_JOB_STORAGE'')');
+cluster = parallel.cluster.Generic('JobStorageLocation', '~/MATLAB_JOB_STORAGE');
+set(cluster, 'HasSharedFilesystem', true);
+set(cluster, 'ClusterMatlabRoot', '/usr/usc/matlab/default/');
+set(cluster, 'OperatingSystem', 'unix');
+
+set(cluster, 'IndependentSubmitFcn', {@independentSubmitFcn, sbatch_arg});
+set(cluster, 'CommunicatingSubmitFcn', {@communicatingSubmitFcn, sbatch_arg});
+set(cluster, 'GetJobStateFcn', @getJobStateFcn);
+set(cluster, 'DeleteJobFcn', @deleteJobFcn);
+
+end

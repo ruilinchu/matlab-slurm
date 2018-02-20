a31e73858942ea1dcfdf81d0a2ef0bac1309e86b
ruilinchu <ruilinchu@gmail.com>
2018-02-20 14:07:13 -0800

    Update and rename exmple_local.m to get_LOCAL_cluster.m

--- get_LOCAL_cluster.m
+++ get_LOCAL_cluster.m
@@ -1,3 +1,4 @@
+function c = get_LOCAL_cluster()
 c=parallel.cluster.Local();
 evalc('system(''export TZ=America/Los_Angeles && mkdir -p ~/MATLAB_JOB_STORAGE_local'')');
 c.JobStorageLocation='~/MATLAB_JOB_STORAGE_local';
@@ -6,8 +7,5 @@ c.JobStorageLocation='~/MATLAB_JOB_STORAGE_local';
 n=str2num(a);
 c.NumWorkers=n;
 
-c.parpool(n)
-%do parfor
-
-delete(gcp)
+end
 

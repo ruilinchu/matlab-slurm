b61a8633cbef11fc74f9358f75fb84a7a6b5d985
ruilinchu <ruilinchu@gmail.com>
2018-02-21 12:16:03 -0800

    Update get_LOCAL_cluster.m

--- get_LOCAL_cluster.m
+++ get_LOCAL_cluster.m
@@ -1,6 +1,7 @@
 function c = get_LOCAL_cluster()
-c=parallel.cluster.Local();
+
 evalc('system(''export TZ=America/Los_Angeles && mkdir -p ~/MATLAB_JOB_STORAGE_local'')');
+c=parallel.cluster.Local();
 c.JobStorageLocation='~/MATLAB_JOB_STORAGE_local';
 
 [a,b]=evalc('system(''nproc --all'')');

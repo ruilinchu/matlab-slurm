522eb384d8d4f54438c12b19eaf0195c0ce0d431
ruilinchu <ruilinchu@gmail.com>
2018-02-21 12:43:11 -0800

    Update get_SLURM_cluster.m

--- get_SLURM_cluster.m
+++ get_SLURM_cluster.m
@@ -1,7 +1,7 @@
 function cluster = get_SLURM_cluster(sbatch_arg)
   % input is slurm sbatch arguments minus ntasks, e.g.:
   % '--time 24:00:00 --partition scec --mem-per-cpu 2G', list goes on
-setenv('TZ','America/Los_Angeles')
+setenv('TZ','America/Los_Angeles') % does not work, better put into bashrc file
 evalc('system(''mkdir -p ~/MATLAB_JOB_STORAGE'')');
 cluster = parallel.cluster.Generic('JobStorageLocation', '~/MATLAB_JOB_STORAGE');
 set(cluster, 'HasSharedFilesystem', true);

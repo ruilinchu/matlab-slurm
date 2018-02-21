a1cdd8ae9f34558db11b055f965d1536649bcffa
ruilinchu <ruilinchu@gmail.com>
2018-02-21 12:22:51 -0800

    Update get_LOCAL_cluster.m

--- get_LOCAL_cluster.m
+++ get_LOCAL_cluster.m
@@ -1,6 +1,7 @@
 function c = get_LOCAL_cluster()
 
-evalc('system(''export TZ=America/Los_Angeles && mkdir -p ~/MATLAB_JOB_STORAGE_local'')');
+setenv('TZ','America/Los_Angeles')
+evalc('system(''mkdir -p ~/MATLAB_JOB_STORAGE_local'')');
 c=parallel.cluster.Local();
 c.JobStorageLocation='~/MATLAB_JOB_STORAGE_local';
 

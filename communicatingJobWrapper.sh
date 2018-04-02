14ed0f24ed54c17ac8aef45e4126d2a751f81f96
ruilinchu <ruilinchu@gmail.com>
2018-04-02 11:08:27 -0700

    Update communicatingJobWrapper.sh

--- communicatingJobWrapper.sh
+++ communicatingJobWrapper.sh
@@ -34,11 +34,21 @@ print $1
 }
 
 cleanupAndExit() {
-    echo "Stopping SMPD over port $SMPD_PORT" 
-    pdsh -f 1 -w $SLURM_NODELIST ${FULL_SMPD} -shutdown -phrase MATLAB -port ${SMPD_PORT}
     rm -fr /tmp/kon1uaLw3sOZnkvg /tmp/kon1uaLw3sOZnkvg2
-    echo "Exiting with code: ${MPIEXEC_CODE}"
-    exit ${MPIEXEC_CODE}
+    echo "Stopping SMPD over port $SMPD_PORT" 
+    pdsh -S -t 30 -u 30 -f 1 -w $SLURM_NODELIST ${FULL_SMPD} -shutdown -phrase MATLAB -port ${SMPD_PORT}
+    if [ $? == 1 ]; then
+        echo
+        echo ============
+        echo SMPD shutdown stalled, job has finished, exiting this job
+        echo ============
+        echo
+        scancel $SLURM_JOB_ID 
+        exit 1
+    else
+        echo "Exiting with code: ${MPIEXEC_CODE}"
+        exit ${MPIEXEC_CODE}
+    fi
 }
 
 launchSmpds() {
@@ -48,7 +58,12 @@ launchSmpds() {
     sleep 60
     pdsh -S -w $SLURM_NODELIST "ps aux | grep ^$USER | grep '[b]in/glnxa64/smpd -phrase MATLAB -port'" &> /dev/null
     if [ $? == 1 ]; then
-        echo SMPD launch failed
+        echo
+        echo ============
+        echo SMPD launch failed, cancelling/exiting this job
+        echo ============
+        echo
+        scancel $SLURM_JOB_ID 
         exit 1
     else
         echo "All SMPDs launched"
@@ -56,13 +71,12 @@ launchSmpds() {
 }
 
 runMpiexec() {
-    # As a debug stage: echo the command line...
-    echo \"${FULL_MPIEXEC}\" -phrase MATLAB -port ${SMPD_PORT} -hosts $SLURM_NNODES ${MACHINE_ARG} -genvlist \
+    echo \"${FULL_MPIEXEC}\" -noprompt -l -exitcodes -phrase MATLAB -port ${SMPD_PORT} -hosts $SLURM_NNODES ${MACHINE_ARG} -genvlist \
         MDCE_DECODE_FUNCTION,MDCE_STORAGE_LOCATION,MDCE_STORAGE_CONSTRUCTOR,MDCE_JOB_LOCATION,MDCE_DEBUG,MDCE_LICENSE_NUMBER,MLM_WEB_LICENSE,MLM_WEB_USER_CRED,MLM_WEB_ID \
         \"${MDCE_MATLAB_EXE}\" ${MDCE_MATLAB_ARGS}
 
     # ...and then execute it
-    eval \"${FULL_MPIEXEC}\" -phrase MATLAB -port ${SMPD_PORT} -hosts $SLURM_NNODES ${MACHINE_ARG} -genvlist \
+    eval \"${FULL_MPIEXEC}\" -noprompt -l -exitcodes -phrase MATLAB -port ${SMPD_PORT} -hosts $SLURM_NNODES ${MACHINE_ARG} -genvlist \
         MDCE_DECODE_FUNCTION,MDCE_STORAGE_LOCATION,MDCE_STORAGE_CONSTRUCTOR,MDCE_JOB_LOCATION,MDCE_DEBUG,MDCE_LICENSE_NUMBER,MLM_WEB_LICENSE,MLM_WEB_USER_CRED,MLM_WEB_ID \
         \"${MDCE_MATLAB_EXE}\" ${MDCE_MATLAB_ARGS}
     MPIEXEC_CODE=${?}
